9a0530f845f07014acf16c12016fcd189ddacbb3
Patryk Lewon <patryklewon@Patryks-MacBook-Air.local>
2023-12-11 22:25:06 +0100

    Add Lines and StopPoints tables to PublicTransportApi
    New migrations have been added to create Lines and StopPoints tables in the PublicTransportApi project. This includes defining the structure for new tables and setting up the database. These tables will be used to store public transport line and stop point information respectively.

---
+++ PublicTransportApi/PublicTransportApi/Migrations/20231211212025_StopPointAdded.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PublicTransportApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class StopPointAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StopPoints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Lat = table.Column<int>(type: "INTEGER", nullable: false),
+                    Long = table.Column<int>(type: "INTEGER", nullable: false),
+                    Identifier = table.Column<string>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StopPoints", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StopPoints");
+        }
+    }
+}
