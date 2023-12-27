477519c120dc0feacaf168d8cf09cb323b55e463
Patryk Lewon <patryklewonn@gmail.com>
2023-12-27 20:54:49 +0100

    Add ScheduleController and improve schedule validation
    A new ScheduleController has been added to handle requests related to schedules. The validation of recurring days for schedule entries has been improved to ensure the schedule adheres to the correct format. Also, the error message for recurring days validation has been updated for better understanding. The ScheduleService has been registered in the Program.cs file to enable it in the application.

--- PublicTransportApi/PublicTransportApi/Program.cs
+++ PublicTransportApi/PublicTransportApi/Program.cs
@@ -6,12 +6,13 @@ using PublicTransportApi.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddApiVersioning();
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("datasource=database.sqlite"));
 builder.Services.AddScoped<ILineService, LineService>();
+builder.Services.AddScoped<IScheduleEntryService, ScheduleService>();
 builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
