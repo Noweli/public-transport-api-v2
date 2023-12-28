01a985ace59efc5148548b970b89c7f6d3c02173
Patryk Lewon <patryklewonn@gmail.com>
2023-12-28 15:01:32 +0100

    Mark collection properties as virtual for lazy loading
    The ICollection properties in "StopPointLineCorrelation", "StopPoint" and "Line" models have been marked as virtual, which allows Entity Framework to use lazy loading for these properties. This optimizes database access by only fetching related data as needed, improving overall performance.

--- PublicTransportApi/PublicTransportApi/Data/Models/StopPointLineCorrelation.cs
+++ PublicTransportApi/PublicTransportApi/Data/Models/StopPointLineCorrelation.cs
@@ -5,5 +5,5 @@ public class StopPointLineCorrelation
     public int Id { get; set; }
     public Line? Line { get; set; }
     public StopPoint? StopPoint { get; set; }
-    public ICollection<ScheduleEntry> ScheduleEntries { get; } = new List<ScheduleEntry>();
+    public virtual ICollection<ScheduleEntry> ScheduleEntries { get; } = new List<ScheduleEntry>();
 }
